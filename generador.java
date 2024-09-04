ef9b16989ac008f1ddec1a04b541d3f9af1156ca
brian <brianarguello358@gmail.com>
2024-09-04 19:18:12 -0400

    completo

--- generador.java
+++ generador.java
@@ -4,7 +4,7 @@ public class generador {
 
 	public void contraseña() {
 		Random random = new Random();
-		String caracteres = "qwertyuiopasdfghjklñzxcvbnm1234567890";
+		String caracteres = "qwertyuiopasdfghjklñzxcvbnm1234567890QWERTYUIOPASDFGHJKLÑZXCVBNM";
 		int contador = 0;
 		int digitos = 10;
 		String Contraseña = "";
@@ -17,12 +17,14 @@ public class generador {
 
 		}
 	
-		System.out.println(Contraseña);
+		System.out.println("Contraseña Generado: " + Contraseña);
 	
 
 	}
 
 	public static void main(String[] args) {
+		
+		System.out.println("***Generador de Contraseñas***\n");
 
 		generador obj = new generador();
 		obj.contraseña();
